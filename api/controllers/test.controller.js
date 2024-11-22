5940f718746c74a2f43f50d539812568902f4752
Aditya Sharma <adityasharmavats2811@gmail.com>
2024-11-22 12:55:31 +0530

    MiddleWare

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,25 +1,29 @@
-import jwt from "jsonwebtoken"
-export const shouldBeLoggedIn = async (req,res) =>{
-  const token = req.cookies.token
+import jwt from "jsonwebtoken";
+export const shouldBeLoggedIn = async (req, res) => {
+  console.log(req.userId)
+  res.status(200).json({
+    message: "You are authenticated",
+  });
+};
 
-  if(!token) return res.status(401).json({
-    message: "Not Authenticated"
-  })
+export const shouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({
+      message: "Not Authenticated",
+    });
 
-  jwt.verify(token,process.env.JWT_SECRET_KEY,async(err,payload)=>{
-    if(err) return res.status(403).json({
-      message:"Token is not valid"
-    })
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err)
+      return res.status(403).json({
+        message: "Token is not valid",
+      });
     if (!payload.isAdmin) {
       return res.status(403).json({ message: "Not authorized!" });
     }
-  })
+  });
 
   res.status(200).json({
-    message:"You are authenticated"
-  })
-}
-
-export const shouldBeAdmin = async (req,res) =>{
-
-}
+    message: "You are authenticated",
+  });
+};
