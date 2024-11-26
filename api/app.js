ebb5a6f13b6391b2342638a6e0da4db58314b753
Aditya Sharma <adityasharmavats2811@gmail.com>
2024-11-26 19:57:30 +0530

    auth_controller and prisma

---
+++ api/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import authRoute from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app =express()
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+
+
+app.listen(8800,()=>{
+  console.log("Server is running")
+})
